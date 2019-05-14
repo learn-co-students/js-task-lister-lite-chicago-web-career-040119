a1e7123dabc7d45ad09ea3f30661a1a43cf7757a
Michael Wnek <beatawnek@Beatas-MacBook-Pro.local>
2019-05-14 14:08:17 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let taskForm = document.getElementById("create-task-form")
+  taskForm.addEventListener("submit", addDiv);
 });
+
+
+
+function addTask() {
+  let task = document.getElementById("new-task-description")
+  let taskElement = document.createElement("li")
+  taskElement.innerHTML = task.value
+  return taskElement
+}
+
+function addDelete() {
+  let deleteButton = document.createElement("button")
+  deleteButton.addEventListener("click", deleteDiv)
+  deleteButton.textContent = "Delete Task"
+  return deleteButton
+}
+
+function addDiv() {
+  event.preventDefault()
+  let list = document.getElementById("tasks")
+  let div = document.createElement("div")
+  list.appendChild(div);
+  div.appendChild(addTask());
+  div.appendChild(addDelete());
+
+}
+
+
+function deleteDiv() {
+  this.parentElement.remove()
+}
